de93078f73eedf5eeceae6240fc24103782b50d4
“preetimca2016” <preetimca2016@gmail.com>
2022-08-11 21:27:55 +0530

    [Preeti] Add: UC1-LineLength

--- LineComparision-Day3/Program.cs
+++ LineComparision-Day3/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace LineComparision_Day3
+﻿using System;
+
+namespace LineComparision_Day3
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            //Line length compute
+            UC1_LineLength line = new UC1_LineLength();
+            line.Length();
+
+            //Line Equality compute
+            UC2_LineEquality equalLine = new UC2_LineEquality();
+            equalLine.Equality();
+
+            //line compare compute
+            UC3_LineCompare compare = new UC3_LineCompare();
+            compare.LineCompare();
         }
     }
 }
---
+++ LineComparision-Day3/UC1_LineLength.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_LineComparision
+{
+    class UC1_LineLength
+    {
+
+        public void Length()
+        {
+            Console.WriteLine("please enter x1 &y1 coordinates: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("please enter x1 &y1 coordinates: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+            double FinalResult = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            {
+                Console.WriteLine("length of line " + FinalResult);
+            }
+
+
+        }
+    }
+}
